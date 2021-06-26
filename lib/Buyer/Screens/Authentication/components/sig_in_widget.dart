124386c95fd3b986347c4d8093215c97ec85c167
65046687 <farazafridi392@gmail.com>
2021-06-26 17:06:03 +0500

    google sign in

---
+++ lib/Buyer/Screens/Authentication/components/sig_in_widget.dart
@@ -0,0 +1,52 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:e_shop/Buyer/Screens/Authentication/components/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LoggedInWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+
+    return Container(
+      alignment: Alignment.center,
+      color: Colors.blueGrey.shade900,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Logged In',
+            style: TextStyle(color: Colors.white),
+          ),
+          SizedBox(height: 8),
+          CircleAvatar(
+            maxRadius: 25,
+            backgroundImage: NetworkImage(user.photoURL),
+          ),
+          SizedBox(height: 8),
+          Text(
+            'Name: ' + user.displayName,
+            style: TextStyle(color: Colors.white),
+          ),
+          SizedBox(height: 8),
+          Text(
+            'Email: ' + user.email,
+            style: TextStyle(color: Colors.white),
+          ),
+          SizedBox(height: 8),
+          ElevatedButton(
+            onPressed: () {
+              final provider =
+                  Provider.of<GoogleSignInProvider>(context, listen: false);
+              provider.logout();
+            },
+            child: Text('Logout'),
+          )
+        ],
+      ),
+    );
+  }
+}
